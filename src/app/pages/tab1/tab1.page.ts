f2ca7755968e2ea3967d848e5a12fe2679d2e564
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-05-22 20:16:44 -0600

    + Test page lifecycle

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   constructor() {}
 
+  ionViewDidEnter() {
+    console.log('Tab 1 ionViewDidEnter');
+  }
+
+  ionViewWillLeave() {
+    console.log('Tab 1 ionViewWillLeave');
+  }
+
+  ionViewDidLeave() {
+    console.log('Tab 1 ionViewDidLeave');
+  }
 }

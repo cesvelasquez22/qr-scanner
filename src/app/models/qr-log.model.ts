a38d35f882a08713a27390f73fbaf9c06493e169
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-05-30 19:52:43 -0600

    + Create model for qr logs

---
+++ src/app/models/qr-log.model.ts
@@ -0,0 +1,34 @@
+export class QrLog {
+  format: string;
+  text: string;
+  type: string;
+  icon: string;
+  created: Date;
+
+  constructor(format: string, text: string) {
+    this.format = format;
+    this.text = text;
+    this.created = new Date();
+
+    this.setType();
+  }
+
+  private setType() {
+    const prefixType = this.text.substr(0, 4);
+    console.log('prefixType', prefixType);
+
+    switch (prefixType) {
+      case 'http':
+        this.type = 'http';
+        this.icon = 'globe-outline';
+        break;
+      case 'geo:':
+        this.type = 'geo';
+        this.icon = 'location-outline';
+        break;
+      default:
+        this.type = 'none';
+        this.icon = 'create';
+    }
+  }
+}

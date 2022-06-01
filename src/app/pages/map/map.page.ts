4ec1d528cc42497326d13e966aad5bd193cf8e30
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-05-31 21:21:28 -0600

    + Show map pilot

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+declare const mapboxgl: any;
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
@@ -26,6 +28,12 @@ export class MapPage implements OnInit {
   }
 
   ngOnInit() {
-    console.log(this.coordinates);
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY2VzdmVsYXNxdWV6IiwiYSI6ImNsM3V6MXM3aTJiZGEzZnBuYnEwa3ZmY2kifQ.B__eMOqegbKFlhRyYt41Rw';
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+    });
   }
 }
